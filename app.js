36b2d71cea4909d0af534e0ea43c6d54be76fb7d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-03 20:54:24 -0300

    add and use the messageDetail router

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
-const indexRouter = require("./src/routes/indexRouter");
+const { indexRouter } = require("./src/routes/indexRouter");
 const newMessageRouter = require("./src/routes/newMessageRouter");
+const messageDetailsRouter = require("./src/routes/messageDetailRouter");
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,7 @@ app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
+app.use("/message", messageDetailsRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
