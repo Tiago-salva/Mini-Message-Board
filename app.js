76c19d4230d5614a49e4e7a4d8fe517ef18df797
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-03 19:39:24 -0300

    configure the app for static files

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const newMessageRouter = require("./src/routes/newMessageRouter");
 
 const app = express();
 const PORT = 3000;
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
