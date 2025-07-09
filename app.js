c267741f59dde9079a110bddee55a5b5f5a3c9ca
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-07-09 10:46:23 -0300

    remove the use of messageDetailRouter and newMessageRouter

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const path = require("path");
 const { indexRouter } = require("./src/routes/indexRouter");
-const newMessageRouter = require("./src/routes/newMessageRouter");
-const messageDetailsRouter = require("./src/routes/messageDetailRouter");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,8 +12,6 @@ app.set("view engine", "ejs");
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
-app.use("/new", newMessageRouter);
-app.use("/message", messageDetailsRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
